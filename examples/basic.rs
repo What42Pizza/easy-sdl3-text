88b118e0a2af5cfc5985bb697f49ac86e27fa780
What42Pizza <burg.what42@gmail.com>
2025-08-26 23:26:22 -0500

    add initial files

---
+++ examples/basic.rs
@@ -0,0 +1,99 @@
+#![feature(duration_constants)]
+
+
+
+use std::time::Instant;
+
+use easy_sdl3_text as sdl3_text;
+use ab_glyph::FontRef;
+use sdl3::pixels::Color;
+pub use sdl3::{render::Canvas, video::Window, event::Event, keyboard::Mod, render::{Texture, TextureCreator}, video::WindowContext};
+pub use anyhow::*;
+
+
+
+struct AppData {
+	should_close: bool,
+}
+
+
+
+fn main() -> Result<()> {
+	
+	// init sdl, window, etc
+	let sdl_context = sdl3::init().unwrap();
+	let video_subsystem = sdl_context.video().unwrap();
+	let window = video_subsystem
+		.window("Text Example", 800, 600)
+		.position_centered()
+		.resizable()
+		.hidden()
+		.build()?;
+	let mut canvas = window.into_canvas();
+	let mut event_pump = sdl_context.event_pump().unwrap();
+	unsafe {
+		sdl3::sys::render::SDL_SetRenderVSync(canvas.raw(), 1);
+	}
+	
+	canvas.present();
+	canvas.window_mut().show();
+	
+	let texture_creator = canvas.texture_creator();
+	let mut text_cache = sdl3_text::TextCache::default();
+	let font = FontRef::try_from_slice(include_bytes!("resources/Inter_24pt-Regular.ttf"))?;
+	
+	let mut data = AppData {
+		should_close: false,
+	};
+	
+	while !data.should_close {
+		
+		for event in event_pump.poll_iter() { handle_event(&mut data, event)?; }
+		
+		draw(&mut canvas, &texture_creator, &mut text_cache, &font)?;
+		
+	}
+	
+	Ok(())
+}
+
+
+
+fn handle_event(data: &mut AppData, event: Event) -> Result<()> {
+	match event {
+		
+		Event::Quit { timestamp: _ } => data.should_close = true,
+		
+		Event::KeyDown { timestamp: _, window_id: _, keycode, scancode: _, keymod, repeat: _, which: _, raw: _ } => {
+			if keycode == Some(sdl3::keyboard::Keycode::W) && (keymod.contains(Mod::RCTRLMOD) || keymod.contains(Mod::LCTRLMOD)) {
+				data.should_close = true;
+			}
+		}
+		
+		_ => {}
+	}
+	Ok(())
+}
+
+
+
+pub fn draw<'a>(canvas: &mut Canvas<Window>, texture_creator: &'a TextureCreator<WindowContext>, text_cache: &mut sdl3_text::TextCache<'a>, font: &FontRef) -> Result<()> {
+	let start = Instant::now();
+	canvas.set_draw_color(Color::RGB(255, 255, 255));
+	canvas.clear();
+	let (width, height) = canvas.output_size()?;
+	let (width, height) = (width as f32, height as f32);
+	
+	let mut size = height * 0.1;
+	let mut y = size;
+	while size > 10.0 {
+		sdl3_text::render_text_subpixel("Example text 1234567890 !@#$%^&*()_+-=[]{}|;:',.<>/?~", size, (height * 0.1) as i32, y as i32, Color::RGB(30, 30, 30), Color::RGB(255, 255, 255), canvas, texture_creator, text_cache, font)?;
+		size *= 0.8;
+		y += size * 1.3;
+	}
+	
+	let millis = start.elapsed().as_millis();
+	if millis > 0 {println!("Draw time exceeds 0 ms: {millis} ms");}
+	canvas.present();
+	Ok(())
+}
---
+++ src/data.rs
@@ -0,0 +1,66 @@
+use std::collections::{HashMap, HashSet};
+use sdl3::{pixels::Color, render::{Texture, TextureValueError, UpdateTextureError}, Error};
+use ab_glyph::Font;
+
+
+
+pub trait ThreadSafeFont: Font + Send + Sync {}
+
+impl<T: Font + Send + Sync> ThreadSafeFont for T {}
+
+// (char, size, foreground, background) -> (texture, width, height, x_offset, y_offset)
+#[derive(Default)]
+pub struct TextCache<'a> {
+	pub(crate) map: HashMap<(char, usize, Color, Color), (Texture<'a>, u32, u32, f32, f32)>,
+	pub(crate) set: HashSet<(char, usize, Color, Color)>,
+}
+
+impl<'a> TextCache<'a> {
+	#[inline]
+	pub fn new() -> Self {
+		Self::default()
+	}
+	pub fn clear(&mut self) {
+		self.map.clear();
+		self.set.clear();
+	}
+}
+
+
+
+#[derive(Debug)]
+pub enum RenderTextError {
+	SdlError (Error),
+	SdlTextureValueError (TextureValueError),
+	SdlUpdateTextureError (UpdateTextureError),
+}
+
+impl From<Error> for RenderTextError {
+	fn from(value: Error) -> Self {
+		Self::SdlError (value)
+	}
+}
+
+impl From<TextureValueError> for RenderTextError {
+	fn from(value: TextureValueError) -> Self {
+		Self::SdlTextureValueError (value)
+	}
+}
+
+impl From<UpdateTextureError> for RenderTextError {
+	fn from(value: UpdateTextureError) -> Self {
+		Self::SdlUpdateTextureError (value)
+	}
+}
+
+impl std::fmt::Display for RenderTextError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::SdlError (err) => write!(f, "Render Text Error: {err}"),
+			Self::SdlTextureValueError (err) => write!(f, "Render Text Error: {err}"),
+			Self::SdlUpdateTextureError (err) => write!(f, "Render Text Error: {err}"),
+		}
+	}
+}
+
+impl std::error::Error for RenderTextError {}
